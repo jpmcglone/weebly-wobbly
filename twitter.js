0a3f05033f45d7e3ae2f27ef85f1f41ec2bf4312
JP McGlone <jp@trifl.co>
2015-05-30 15:08:22 -0700

    table flip

--- twitter.js
+++ twitter.js
@@ -1,5 +1,13 @@
 jQuery(function($) {
     console.log("doc ready!");
-
-    // TODO: set classes based on the hashtag found on the twitter account
+    
+	function tableFlip() {
+        console.log("Table Flip!");
+		$("div").addClass("box_rotate box_transition");
+	}
+	
+	$(document).ready(function() {
+		tableFlip()
+		// TODO: set classes based on the hashtag found on the twitter account
+    });
 });
