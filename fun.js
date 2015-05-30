0cb50fabf4ffe85e679e6026c566fcb195c09ef5
JP McGlone <jp@trifl.co>
2015-05-30 16:01:24 -0700

    put a delay in tableflip, still need to get the tableflip guy to show up first:

---
+++ fun.js
@@ -0,0 +1,15 @@
+jQuery(function($) {
+    console.log("doc ready!");
+    
+	function tableFlip() {
+        console.log("Table Flip!");
+        // TODO: show table flip guy first
+        setTimeout(function() {
+		  $("div").addClass("box_rotate box_transition")
+        }, 800);
+	}
+	
+	$(document).ready(function() {
+		tableFlip()
+    });
+});
