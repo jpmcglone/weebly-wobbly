3119f239c017c8de8d7d809b737031c8fa0ca2e7
theluxury <zhengkaw@gmail.com>
2015-05-31 10:31:14 -0700

    rick roll speed

--- themes/rickRoll/rick-roll.js
+++ themes/rickRoll/rick-roll.js
@@ -11,7 +11,7 @@ var changesRR = function() {
   audio = new Audio('http://upload.wikimedia.org/wikipedia/en/d/d0/Rick_Astley_-_Never_Gonna_Give_You_Up.ogg')
   audio.play()
 
-  $('body').append('<marquee id="giant-head" behavior="scroll" direction="left">' +
+  $('body').append('<marquee id="giant-head" behavior="scroll" direction="left" scrollamount="9">' +
     '<img src="http://31.media.tumblr.com/c4b03d9214b2f1cb506b5fc2d7d0deb7/tumblr_mgktubqPt51r933bpo1_400.gif" width="400" alt="rickface">' +
   '</marquee>')
   window.setTimeout(undoRR, 15000)
