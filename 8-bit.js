40d86c6540077b3286a3d3b1cf2bbc66a3c1f175
Harry Moreno <morenoh149@gmail.com>
2015-05-30 20:11:15 -0700

    fix flower on 8bit

--- 8-bit.js
+++ 8-bit.js
@@ -7,7 +7,7 @@ var undo8Bit = function() {
  * 8-bit theme specific javascript
  */
 var changes8Bit = function() {
-  $('body').append('<img style="position: fixed; width: 200px; height: 200px; right: 0"' +
+  $('body').append('<img style="position: fixed; width: 200px; height: 200px; right: 0; bottom: 10px"' +
                    'class="eight-bit" src="files/theme/images/dundundun.gif">')
   window.setTimeout(undo8Bit, 15000)
 }
