a5ee249188d73e6861bbf65e085dfa44f544502f
Harry Moreno <morenoh149@gmail.com>
2015-05-30 20:58:26 -0700

    display twitter user id in lower left corner

--- twitter-puller.js
+++ twitter-puller.js
@@ -5,33 +5,29 @@ jQuery(function($) {
 	// The stuff from data is username, message_id, message_text, user_pic, and message_hashtag
 	// The allowed hashtags are 'wobblycats', 'wobblyrickroll', 'wobblytableflip', 
 	// 'wobbly8bit', 'wobblyallyourbase'
-
 	function getLatestTweet() {
 		$.get( "https://quiet-bastion-3290.herokuapp.com/twitter_puller", function( data ) {
-			console.log(data.message_hashtag);
-  			if (data.message_id == lastMessageId) {
-  				console.log("same");
-  				return;
-  			}
-  			lastMessageId = data.message_id;
-  			changeCssHere(data.message_hashtag);
-            showTweet(data)
+			console.log(data.message_hashtag)
+      if (data.message_id == lastMessageId) {
+        console.log("same")
+        return
+      }
+      lastMessageId = data.message_id
+      changeCssHere(data.message_hashtag)
+      showTweet(data)
 		})
 	}
 
-    function showTweet(data) {
-        console.log("showing tweet")
-        console.log(data.message_id)
-        console.log(data.username)
-        console.log(data.user_pic)
-        console.log(data.message_id)
-        console.log(data.message_text)
-        console.log(data.message_hashtag)
+  function showTweet(data) {
+    console.log("show tweet")
 
-        // Show div
-
-        // Hide div
-    }
+    $('body').append('<div id="twitter-user" style="position: fixed; left: 0; bottom: 10px; width: 300px; height: 50px">' +
+                     data.username +
+                     '</div>')
+    window.setTimeout(function() {
+      $('div#twitter-user').remove()
+    }, 15000)
+  }
 
 	function changeCssHere(string) {
 		if (string == 'wobbly8bit') {
