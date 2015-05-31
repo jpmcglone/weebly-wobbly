29925534ee085b44463c44090bc2c32e77f07397
Harry Moreno <morenoh149@gmail.com>
2015-05-31 03:52:01 -0700

    fix refs, factored out audio from changer.js

--- themes/eightBit/8-bit.js
+++ themes/eightBit/8-bit.js
@@ -7,6 +7,10 @@ var undo8Bit = function() {
  * 8-bit theme specific javascript
  */
 var changes8Bit = function() {
+  audio.pause()
+  audio = new Audio('http://themushroomkingdom.net/sounds/wav/smb/smb_world_clear.wav')
+  audio.play()
+
   $('body').append('<img style="position: fixed; width: 200px; height: 200px; right: 0; bottom: 10px"' +
                    'class="eight-bit" src="files/theme/themes/eightBit/pipeFlower.gif">')
   window.setTimeout(undo8Bit, 15000)
--- themes/rickRoll/rick-roll.js
+++ themes/rickRoll/rick-roll.js
@@ -7,6 +7,10 @@ var undoRR = function() {
  * rick-roll theme specific javascript
  */
 var changesRR = function() {
+  audio.pause()
+  audio = new Audio('http://upload.wikimedia.org/wikipedia/en/d/d0/Rick_Astley_-_Never_Gonna_Give_You_Up.ogg')
+  audio.play()
+
   $('body').append('<marquee id="giant-head" behavior="scroll" direction="left">' +
     '<img src="http://31.media.tumblr.com/c4b03d9214b2f1cb506b5fc2d7d0deb7/tumblr_mgktubqPt51r933bpo1_400.gif" width="600" alt="rickface">' +
   '</marquee>')
