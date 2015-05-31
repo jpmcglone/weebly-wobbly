f20f55493075cf09ae4c5b5fb4d61aec0a424d73
JP McGlone <jp@trifl.co>
2015-05-30 17:52:35 -0700

    added to tableflip

--- fun.js
+++ fun.js
@@ -3,16 +3,27 @@ jQuery(function($) {
     
 	function tableFlip() {
         console.log("Table Flip!");
+        var audio = new Audio('doitlive.mp3');
+        audio.play();
         // TODO: show table flip guy first
         setTimeout(function() {
-          $("#banner").addClass("table-flip")
+          $("#banner").addClass("table-flip");
           setTimeout(function() {
-		      $("div").addClass("box_rotate box_transition")
+		      $("div").addClass("box_rotate box_transition");
           }, 250);
         }, 2000);
 	}
+    
+    function undoTableFlip() {
+        $("#banner").removeClass("table-flip");
+        $("div").removeClass("box_rotate box_transition");
+    }
 	
 	$(document).ready(function() {
-		tableFlip()
+		//tableFlip()
+        
+//        setTimeout(function() {
+//            undoTableFlip()
+//        }, 4000);
     });
 });
