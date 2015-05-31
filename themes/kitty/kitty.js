4f9bce17d8af2a8913048a03fa8814b2c4a2be12
theluxury <zhengkaw@gmail.com>
2015-05-31 09:27:04 -0700

    kitty theme

--- themes/kitty/kitty.js
+++ themes/kitty/kitty.js
@@ -10,7 +10,8 @@ var changesKitty = function() {
   audio.pause()
   audio = new Audio('files/theme/themes/kitty/Meow_Mix_Commercial.mp3')
   audio.play()
+
   $('body').append('<img style="position: fixed; width: 200px; height: 200px; right: 0; bottom: 10px"' +
-                   'class="kitty">')
+                   'class="kitty" src="http://media.giphy.com/media/5ZxSN5D9rcUI8/giphy.gif">')
   window.setTimeout(undoKitty, 15000)
 }
