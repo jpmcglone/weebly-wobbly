fe134c36d704b11e057450566917b03af69a7f28
Harry Moreno <morenoh149@gmail.com>
2015-05-30 20:41:23 -0700

    reenable polling

--- twitter-puller.js
+++ twitter-puller.js
@@ -1,10 +1,10 @@
 jQuery(function($) {
 
 	var lastMessageId;
-    
+
 	// The stuff from data is username, message_id, message_text, user_pic, and message_hashtag
 	// The allowed hashtags are 'wobblycats', 'wobblyrickroll', 'wobblytableflip', 
-	//		'wobbly8bit', 'wobblyallyourbase'
+	// 'wobbly8bit', 'wobblyallyourbase'
 
 	function getLatestTweet() {
 		$.get( "https://quiet-bastion-3290.herokuapp.com/twitter_puller", function( data ) {
@@ -16,9 +16,9 @@ jQuery(function($) {
   			lastMessageId = data.message_id;
   			changeCssHere(data.message_hashtag);
             showTweet(data)
-		});
+		})
 	}
-    
+
     function showTweet(data) {
         console.log("showing tweet")
         console.log(data.message_id)
@@ -27,23 +27,23 @@ jQuery(function($) {
         console.log(data.message_id)
         console.log(data.message_text)
         console.log(data.message_hashtag)
-        
+
         // Show div
-        
-        // Hide div 
+
+        // Hide div
     }
-    
+
 	function changeCssHere(string) {
 		if (string == 'wobbly8bit') {
-			changer('eight-bit', changes8Bit);
+			changer('eight-bit', changes8Bit)
 		} else if (string == 'wobblyrickroll') {
-			changer('rick-roll', changesRR);
+			changer('rick-roll', changesRR)
 		} else if (string == 'wobblytableflip') {
-			changer('table-flip', tableFlip);
+			changer('table-flip', tableFlip)
 		}
 	}
 
 	$(document).ready(function() {
-		setInterval(getLatestTweet, 10000);
-    });
-});
+		setInterval(getLatestTweet, 10000)
+    })
+})
