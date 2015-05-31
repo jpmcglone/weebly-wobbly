4cdf5ee2d0ff2fed026cb447ee4c6736b38daaf6
theluxury <zhengkaw@gmail.com>
2015-05-30 23:47:58 -0700

    kitty

--- kitty.js
+++ kitty.js
@@ -7,6 +7,8 @@ var undoKitty = function() {
  * kitty theme specific javascript
  */
 var changesKitty = function() {
+	var audio = new Audio('files/theme/ie_Short_Cow_MooMeow_Mix_Comm-KevanGC-2007570186.mp3')
+  	audio.play()
   $('body').append('<img style="position: fixed; width: 200px; height: 200px; right: 0; bottom: 10px"' +
                    'class="kitty">')
   window.setTimeout(undoKitty, 15000)
