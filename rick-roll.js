db491d4d46d90581c6ced4c781945f66abac53c2
Harry Moreno <morenoh149@gmail.com>
2015-05-30 20:11:15 -0700

    fix flower on 8bit

--- rick-roll.js
+++ rick-roll.js
@@ -1,4 +1,5 @@
 var undoRR = function() {
+  $('marquee').remove()
   changer('')
 }
 
@@ -6,5 +7,8 @@ var undoRR = function() {
  * rick-roll theme specific javascript
  */
 var changesRR = function() {
+  $('body').append('<marquee behavior="scroll" direction="left">' +
+    '<img src="http://31.media.tumblr.com/c4b03d9214b2f1cb506b5fc2d7d0deb7/tumblr_mgktubqPt51r933bpo1_400.gif" width="600" alt="rickface">' +
+  '</marquee>')
   window.setTimeout(undoRR, 15000)
 }
